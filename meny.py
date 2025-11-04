a0cc340f20afaad18def52b814cd6683a34d2d8b
Jonas54311 <jonas.holmberg@elev.ga.ntig.se>
2025-11-04 13:38:09 +0100

    guhib

--- meny.py
+++ meny.py
@@ -1,15 +1,22 @@
-run = True
-print("Välkommen till påsen")
 bag = []
-while run:
+print("Välkommen till påsen")
+while True:
     print("Visa inehållet [V]")
     print("Spara i påsen [S]")
-    print("")
+    print("Ta bort från påsen [R]")
     print("Avsluta programmet [Q]")
     choice = input("Vad vill du göra ")
     if choice.lower() == "v":
-        print(bag)
-    elif choice.lower() == "s":
+        for i in range(len(bag)):
+            print(bag[l])
+        input("Tryck [ENTER] för att fortsätta")
+    elif choice.lower() == "s" and len(bag) < 10:
         bag.append(input("Skriv vad du vill spara "))
+    elif len(bag) == 10:
+        print("Påsen är full")
+    elif choice.lower() == "r":
+        bag.remove(input("Skriv vad du vill ta bort"))
     elif choice.lower() == "q":
-        run = False
+        break
+    for i in range(5):
+        print("")
