2f94e932ff7e8bae12f70ea8ca28dcf283330e45
Jonas54311 <jonas.holmberg@elev.ga.ntig.se>
2025-11-14 22:23:28 +0100

    fhcgvjh

--- meny.py
+++ meny.py
@@ -3,20 +3,26 @@ from random import randint
 def forts():
     input("Tryck [ENTER] för att fortsätta")
 symbol = ["\u2780", "\u2781", "\u2782", "\u2783", "\u2784", "\u2785", "\u2786", "\u2787", "\u2788", "\u2789"]
-bag = [""]
+bag = ["Påsen är tom"]
 ätit = ["Det var gott", "Det var inte gott"]
-mellanrum = 2
-första = True
-print("Välkommen till påsen")
+print("           .¨.")
+print("            ¨ ")
+print(":::::.    .:::.   .::'::.  :::::::  ::.  ::")
+print("::   ::  .:: ::.  ::.  ''  ::       :::. ::")
+print(":::::'   ::...::   ':::.   :::::    ::':.::")
+print("::       ::'''::  ..  '::  ::       :: ':::")
+print("::       ::   ::  '::.::'  :::::::  ::  '::")
+print(" \n")
+forts()
 while True:
-    print("Visa inehållet [V]")
-    print("Spara i påsen [S]")
+    print("Visa inehållet     [V]")
+    print("Spara i påsen      [S]")
     print("Ta bort från påsen [R]")
-    print("Sök i påsen [F]")
-    print("Ät ur påsen [E]")
+    print("Sök i påsen        [F]")
+    print("Ät ur påsen        [E]")
+    print("Töm påsen          [T]")
     print("Avsluta programmet [Q]")
-    for i in range(mellanrum):
-        print("")
+    print("\n")
     choice = input("Vad vill du göra ")
     if choice.lower() == "v":
         for i in range(len(bag)):
@@ -24,21 +30,19 @@ while True:
         forts()
     elif choice.lower() == "s" and len(bag) < 10:
         bag.append(input("Skriv vad du vill spara "))
-        if första:
+        if bag[0] == "Påsen är tom":
             bag.pop(0)
-            första = False
     elif len(bag) == 10:
         print("Påsen är full")
         forts()
-    elif choice.lower() == "r": 
-        if första:
-            print("Påsen är tom")
-        else:
-            for i in range(len(bag)):
-                print(symbol[i], bag[i])
-            rem = int(input("Skriv siffran till den du vill ta bort"))
-            print(f"{bag[rem-1]} har tagits bort")
-            bag.pop(rem-1)
+    elif choice.lower() == "r" and bag[0] == "Påsen är tom":
+        print(bag[0])
+    elif choice.lower() == "r" and bag[0] != "påsen är tom": 
+        for i in range(len(bag)):
+            print(symbol[i], bag[i])
+        rem = int(input("Skriv siffran till den du vill ta bort"))
+        print(f"{bag[rem-1]} har tagits bort")
+        bag.pop(rem-1)
         forts()
     elif choice.lower() == "f":
         query = input("vad vill du söka ")
@@ -55,6 +59,31 @@ while True:
         else:
             print(f"Det fanns inte {meal}")
         forts()
+    elif choice.lower() == "t":
+        bag = ["Påsen är tom"]
+        print("Påsen är nu tom")
+        forts()
+    elif choice.lower() == "b":
+        print("   *.''''.   ..")
+        print("  *:      '.:::::.")
+        print(" * :     .:::::::::.")
+        print("  *    ':::::::::::::.")
+        print("  (       ':::::::::::::.")
+        print(" (*)        ':::::::::::::.")
+        print(" :             ':::::::::::::.")
+        print(":                 ':::::::::::::.")
+        boom = input("Är du säker på att du vill spränga påsen? (y/n) ")
+        if boom.lower() == "y":
+            print("         ::         ")
+            print("         ::         ")
+            print("         ::         ")
+            print("       .::::.       ")
+            print("::::::::::::::::::::")
+            print("       '::::'       ")
+            print("         ::         ")
+            print("         ::         ")
+            print("         ::         ")
+            print("         ::         ")
+            break
     elif choice.lower() == "q":
         break
-    mellanrum = 3
